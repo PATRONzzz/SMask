a21600edc77df026741d679d3376367509938f08
PATRONzzz <androsov.artem.v@gmail.com>
2025-04-15 21:50:51 +1100

    schemas

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,22 @@
+from typing import Generator
+
 from smask.config import setting
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = setting.DATABASE_URL
-print("Database URL is ", SQLALCHEMY_DATABASE_URL)
+print(
+    "Database URL is ", SQLALCHEMY_DATABASE_URL
+)  # Временная строка, нужно удалить
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal
+        yield db
+    finally:
+        db.close()
